6b9f0a52d8f99d1fc1ba7476875c7d8687f35b92
mamuka111 <123620275+mamuka111@users.noreply.github.com>
2023-03-22 13:46:22 +0400

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -4,7 +4,27 @@ function App() {
   return (
 
   <>
-  <h1>MAMUKA</h1>
+  <div className='mainDiv'>
+  <div className='leftDiv'>
+    <div className='leftCompDiv'>
+    <div className='learnToCode'>Learn to code by watching others</div>
+    <div className='seeHow'>See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable. </div>
+    </div>
+  </div>
+  <div className='rightDiv'>
+    <div className='tryItFree'>
+      <div className='tryItFreeChild'><span className='days7'>Try it free 7 days</span> <span className='then'> then $20/mo.thereafter</span> </div>
+    </div>
+    <div className='signUp'>
+      <div className='firstName'></div>
+      <div className='lastName'></div>
+      <div className='email'></div>
+      <div className='password'></div>
+      <div className='submit'><p className='submitChild'>CLAIM YOUR FREE TRIAL</p></div>
+      <div className='termsAndServices'><span className='termsAndServices1'>By clicking the button, you are agreeing to our</span><span className='termsAndServices2'> Terms and Services</span></div>
+    </div>
+  </div>
+  </div>
   </>
   )
 }
